ee22112b625111d579a415cba754168c1a797a4e
Diego Restrepo <diego.restrepo@ucdenver.edu>
2019-09-28 04:05:55 -0600

    drg_ranksum_or_ttest was modified

--- drg_ranksum_or_ttest.m
+++ drg_ranksum_or_ttest.m
@@ -5,23 +5,27 @@ function [p_val,r_or_t] = drg_ranksum_or_ttest(x,y)
 %r_or_t=2 - t test
 %   Detailed explanation goes here
       if (length(x)<4)||(length(y)<4)
-                %adtest does not work with n<4. In that case go the
-                %safe way ranksum
-                p_val=ranksum(x,y);
-                r_or_t=0;
-            else
-                if (adtest(x)==1)||(adtest(x)==1)
-                    p_val=ranksum(x,y);
-                    r_or_t=0;
-                else
-                    if length(x)~=length(y)
-                        [h, p_val]=ttest2(x,y);
-                        r_or_t=1;
-                    else
-                        [h, p_val]=ttest(x,y);
-                        r_or_t=2;
-                    end
-                end
-            end
+          %adtest does not work with n<4. In that case go to ttest
+          if length(x)~=length(y)
+              [h, p_val]=ttest2(x,y);
+              r_or_t=1;
+          else
+              [h, p_val]=ttest(x,y);
+              r_or_t=2;
+          end
+      else
+          if (adtest(x)==1)||(adtest(x)==1)
+              p_val=ranksum(x,y);
+              r_or_t=0;
+          else
+              if length(x)~=length(y)
+                  [h, p_val]=ttest2(x,y);
+                  r_or_t=1;
+              else
+                  [h, p_val]=ttest(x,y);
+                  r_or_t=2;
+              end
+          end
+      end
 end
 
